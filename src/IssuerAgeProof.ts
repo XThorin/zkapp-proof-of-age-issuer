57ebf590ac3cf80eadf689b7048899635b1fe876
Bugrahan Temuer <bugrahan.temur@gmail.com>
2022-11-01 21:55:46 +0100

    Make class constants

--- src/IssuerAgeProof.ts
+++ src/IssuerAgeProof.ts
@@ -13,27 +13,31 @@ export { IssuerAgeProof_ };
 class IssuerAgeProof_ extends SmartContract {
   @state(PublicKey) stateAddress = State<PublicKey>();
 
+  readonly ageToProve = 18;
+  readonly personalEncryptedAge =
+    Field.fromBigInt(
+      21294934938834819506687011406629737744121517272917194830529619446796258228081n
+    );
+
   @method giveAnswer(answer: Field, address: PublicKey) {
-    const ageToProve = 18;
-
+    // Compute hash chain by going [ageToProve] steps over the user's answer
     let hashChainValue = answer;
-
-    for (let i = 0; i < ageToProve; ++i) {
+    for (let i = 0; i < this.ageToProve; ++i) {
       hashChainValue = Poseidon.hash([hashChainValue]);
     }
 
-    // solution (age encryption provided by the issuer)
-    // check if the provided age is true
-    let solution =
-      Field.fromBigInt(
-        21294934938834819506687011406629737744121517272917194830529619446796258228081n
-      );
+    // personalEncryptedAge (age encryption provided by the identity issuer)
+    // Check if the the user's answer is truthful and proves age > [ageToProve].
+    hashChainValue.assertEquals(this.personalEncryptedAge);
 
-    hashChainValue.assertEquals(solution);
+    // If the true age is < [ageToProve], assertion will fail
+    // even if the user provided an answer calculated
+    // by true credentials.
 
-    // if assertion passes, update state
-    let value_ = this.stateAddress.get();
-    this.stateAddress.assertEquals(value_);
+    // If the assertion passes, update the zkApp's state.
+    // New state is the public key of the account who sent the right answer.
+    const stateAddress = this.stateAddress.get();
+    this.stateAddress.assertEquals(stateAddress);
     this.stateAddress.set(address);
   }
 }
