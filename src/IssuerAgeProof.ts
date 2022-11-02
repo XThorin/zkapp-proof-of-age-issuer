e5d6f66660ceebe60cc32c9d6cda4426e7da6359
Bugrahan Temuer <bugrahan.temur@gmail.com>
2022-11-02 22:18:14 +0100

    Add comments and fix config

--- src/IssuerAgeProof.ts
+++ src/IssuerAgeProof.ts
@@ -14,6 +14,16 @@ class IssuerAgeProof_ extends SmartContract {
   @state(PublicKey) stateAddress = State<PublicKey>();
 
   readonly ageToProve = 18;
+
+  // This is a cryptographic hash that will be different for zkApps deployed for
+  // different persons. The answer that the user gives as the input to the smart
+  // contract is checked against this solution provided by the identity issuer,
+  // who is trusted to deliver truthful information about a person's identity.
+  // The issuer might for example arrive at this value by hashing a person's
+  // ID number as many as times as their age + 1. The value below was calculated
+  // for a person with (ID number: 19811843 Age: 58).
+  // The assertions will only pass if an answer is provided to the *giveAnswer*
+  // method that is calculated using these two pieces of information.
   readonly personalEncryptedAge =
     Field.fromBigInt(
       21294934938834819506687011406629737744121517272917194830529619446796258228081n
@@ -31,8 +41,7 @@ class IssuerAgeProof_ extends SmartContract {
     hashChainValue.assertEquals(this.personalEncryptedAge);
 
     // If the true age is < [ageToProve], assertion will fail
-    // even if the user provided an answer calculated
-    // by true credentials.
+    // even if the user provided an answer calculated with true credentials.
 
     // If the assertion passes, update the zkApp's state.
     // New state is the public key of the account who sent the right answer.
